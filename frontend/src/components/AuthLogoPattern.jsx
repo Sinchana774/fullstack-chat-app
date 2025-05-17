ef6163e89c1cb74d52d4137bc0f2f8299a04fbbf
Sinchana774 <sinchanahu07@gmail.com>
2025-05-18 01:08:41 +0530

    Update AuthLogoPattern.jsx

--- frontend/src/components/AuthLogoPattern.jsx
+++ frontend/src/components/AuthLogoPattern.jsx
@@ -2,7 +2,7 @@ const AuthLogoPattern = ({ title, subtitle }) => {
     return (
       <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
         <div className="max-w-md text-center">
-          <img src="https://res.cloudinary.com/dvfkurhis/image/upload/v1743365752/wdojfh0ceibgbdkkjqxs.png"></img>
+          <img src="https://res.cloudinary.com/dvfkurhis/image/upload/v1747510468/Echo_fyfbje.png"></img>
           <br/>
           <h2 className="text-2xl font-bold mb-4">{title}</h2>
           <p className="text-base-content/60">{subtitle}</p>
